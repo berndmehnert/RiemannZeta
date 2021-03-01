3e7138b1751ca897fd4b9e6f5dc77167587234b9
bernd <bernd.mehnert@artengis.com>
2021-03-01 10:25:16 +0100

    small change

--- src/RiemannZeta.jl
+++ src/RiemannZeta.jl
@@ -1,7 +1,7 @@
 module RiemannZeta
 export SimpleRiemann
 
-# s = σ + it, first non-trivial zeros at s = 0.5 + i14.135
+# s = σ + it, first non-trivial zeros at s = 0.5 +/- i14.135
 function SimpleRiemann(s, ae = 0.5)
     N = floor(1/(ae/(1 + abs(s)/real(s)))^(1/real(2))) + 1
     return sum([n^(-s) for n in 1:N]) + N^(1-s)/(s-1)
