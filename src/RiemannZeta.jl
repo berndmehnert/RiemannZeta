e70f5de558f3c985631753337858257fdb4f2330
bernd <bernd.mehnert@artengis.com>
2021-03-01 10:22:55 +0100

    simple zeta function values

--- src/RiemannZeta.jl
+++ src/RiemannZeta.jl
@@ -1,8 +1,10 @@
 module RiemannZeta
 export SimpleRiemann
 
-function SimpleRiemann(z, N = 100)
-    return sum([n^(-z) for n in 1:N]) + N^(1-z)/(z-1)
+# s = σ + it, first non-trivial zeros at s = 0.5 + i14.135
+function SimpleRiemann(s, ae = 0.5)
+    N = floor(1/(ae/(1 + abs(s)/real(s)))^(1/real(2))) + 1
+    return sum([n^(-s) for n in 1:N]) + N^(1-s)/(s-1)
 end
 
 end # module
