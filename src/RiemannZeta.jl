56189b6791b053d19fce36be7928907a164786b9
bernd.h.mehnert <bernd.h.mehnert@gmail.com>
2021-04-06 08:27:20 +0200

    changed constants

--- src/RiemannZeta.jl
+++ src/RiemannZeta.jl
@@ -3,7 +3,7 @@ using Images
 export SimpleRiemann, GetImageOfCriticalStrip
 
 # s = σ + it, first non-trivial zeros at s = 0.5 +/- i14.135
-function SimpleRiemann(s, ae = 0.00001)
+function SimpleRiemann(s, ae = 0.0001)
     N = floor(1/(ae/(1 + abs(s)/real(s)))^(1/real(2))) + 1
     return sum([n^(-s) for n in 1:N]) + N^(1-s)/(s-1)
 end
